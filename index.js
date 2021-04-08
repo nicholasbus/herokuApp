e82ebd6ec43304b83a7b68b2de82485d5a5ad6e6
Nicholas Smith <nicholassmithbus@gmail.com>
2021-04-08 11:41:04 -0600

    push

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 9000;
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use("/api/", (_, res) => {
   res.json({ data: "API is accounted for" });
